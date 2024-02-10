e052f5b28d8e0b4b2641027cf49eef9a2879c30a
DiamondraAndriam <118435008+DiamondraAndriam@users.noreply.github.com>
2024-02-10 07:04:34 +0300

    déplacement de Annonces

---
+++ src/pages/Annonces.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Menu from "../component/menu";
+import Form from "../component/Form";
+import Card from "../component/Card"; // Assurez-vous d'importer le composant Card
+
+function Annonces() {
+  // Liste des données de cartes
+  const cardData = [
+    { title: "Titre 1", description: "Description 1", price: "1000" },
+    { title: "Titre 2", description: "Description 2", price: "2000" },
+    { title: "Titre 3", description: "Description 3", price: "3000" },
+    { title: "Titre 1", description: "Description 1", price: "1000" },
+    { title: "Titre 1", description: "Description 1", price: "1000" },
+    { title: "Titre 1", description: "Description 1", price: "1000" },
+    { title: "Titre 2", description: "Description 2", price: "2000" },
+    { title: "Titre 3", description: "Description 3", price: "3000" },
+    { title: "Titre 1", description: "Description 1", price: "1000" },
+    { title: "Titre 1", description: "Description 1", price: "1000" },
+    { title: "Titre 1", description: "Description 1", price: "1000" },
+    { title: "Titre 2", description: "Description 2", price: "2000" },
+    { title: "Titre 3", description: "Description 3", price: "3000" },
+    { title: "Titre 1", description: "Description 1", price: "1000" },
+  ];
+
+  return (
+    <div>
+      <Menu />
+      <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", columnGap: "20px" }}>
+        <Form />
+        <div style={{ marginTop: '100px', marginLeft: '20px',display: "grid", gridTemplateColumns: "1fr 1fr 1fr 1fr", columnGap: "20px" }}>
+          {cardData.map((card, index) => (
+            <Card key={index} title={card.title} description={card.description} price={card.price} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Annonces;
